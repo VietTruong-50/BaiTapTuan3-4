d44e41af87fe11e96399eecf0c473277b6258f83
VietTruong-50 <65813680+VietTruong-50@users.noreply.github.com>
2021-10-27 21:15:01 +0700

    Update BaiTap7.java

--- edu/tuan3/BaiTap7.java
+++ edu/tuan3/BaiTap7.java
@@ -1,42 +1,24 @@
 package edu.tuan3;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class BaiTap7 {
 	public static void main(String[] args) {
-	    int number;
-	    ArrayList<Integer> arrListNumber = new ArrayList<>();
-	    Scanner scanner = new Scanner(System.in);
-	         
-	    for (int i = 0; i < 10; i++) {
-	        System.out.print("Nhập phần tử thứ " + i + ": ");
-	        number = scanner.nextInt();
-	        arrListNumber.add(number);
-	    }
-	         
-	    // tạo 1 ArrayList arrTemp
-	    ArrayList<Integer> arrTemp = new ArrayList<>();
-	         
-	    // thêm các phần tử của arrListNumber vào arrTemp
-	    // nếu trong arrTemp đã tồn tại phần tử giống trong arrListNumber
-	    // thì không thêm vào, ngược lại thêm bình thường
-	    for (int i = 0; i < arrListNumber.size(); i++) {
-	        if (!arrTemp.contains(arrListNumber.get(i))) {
-	            arrTemp.add(arrListNumber.get(i));
-	        }
-	    }
-	         
-	    // xóa các phần tử của arrListNumber
-	    arrListNumber.clear();
-	         
-	    // thêm tất cả các phần tử của arrTemp vào arrListNumber
-	    // lúc này ta sẽ có 1 ArrayList arrListNumber
-	    // không chứa các phần tử trùng nhau
-	    arrListNumber.addAll(arrTemp);
-	         
-	    System.out.println("Các phần tử có trong arrListNumber là: ");
-	    System.out.println(arrListNumber);
+		int arr[] = { 2, 5, 8, 3, 0, 9, 0, 12, 17, 22, 95, 11, 0};
+		int min=arr[0];
+		boolean isExist = false;
+		int n =0;
+		for (int i = 0; i < arr.length; i++) {
+			isExist = false;
+			for (int j = i; j <arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					isExist = true;
+				}
+			}
+			if (min > arr[i] && !isExist) {
+				min = arr[i];
+			}
+		}
+
+		System.out.println(min);
+		System.out.println(" Truong Quoc Viet 20198341");
 	}
 }
