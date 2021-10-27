e5947e90de7dc77efe407d982361c683b64a6e12
VietTruong-50 <65813680+VietTruong-50@users.noreply.github.com>
2021-10-27 21:16:28 +0700

    Update BaiTap8.java

--- edu/tuan3/BaiTap8.java
+++ edu/tuan3/BaiTap8.java
@@ -7,16 +7,8 @@ public class BaiTap8 {
 		int arr[] = { 5, 2, 8, 3, 0, 9, 0, 12, 17, 22, 95, 11, 0 };
 		int[] brr = new int[arr.length];
 		int bSize = 0;
-		boolean isExist = false;
 		for (int i = 0; i < arr.length; i++) {
-			isExist = false;
-			for (int j = 0; j < bSize; j++) {
-				if (arr[i] == brr[j]) {
-					isExist = true;
-					break;
-				}
-			}
-			if (!isExist && arr[i] != 0) {
+			if ( arr[i] != 0) {
 				brr[bSize++] = arr[i];
 			}
 		}
