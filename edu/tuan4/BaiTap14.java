22225477a86ff9ee816cb5d338bfc44b6eb642e9
VietTruong-50 <65813680+VietTruong-50@users.noreply.github.com>
2021-10-31 20:48:24 +0700

    Update BaiTap14.java

--- edu/tuan4/BaiTap14.java
+++ edu/tuan4/BaiTap14.java
@@ -1,10 +1,28 @@
 package edu.tuan4;
 
+import java.util.Scanner;
+
 public class BaiTap14 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String S1 = "1111100000";
+		char str[] = S1.toCharArray();
+		int j = 0;
+		int n = sc.nextInt();
 
+		for (j = 1; j < n; j++) {
+			int i = 1;
+			while (i < 10) {
+				char tmp = str[i - 1];
+				str[i - 1] = str[i];
+				str[i] = tmp;
+				i++;
+			}
+		}
+		String Sk = new String(str);
+		System.out.print("xau thu " + n + " la : " + Sk);
+		System.out.println("\nTruong Quoc Viet 20198341");
 	}
 
 }
